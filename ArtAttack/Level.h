fb972635d3a56f5a32cddefbe378218e1cbe5178
matt439 <120173618+matt439@users.noreply.github.com>
2024-08-30 14:46:59 +1000

    deleted menu files

--- ArtAttack/Level.h
+++ ArtAttack/Level.h
@@ -27,7 +27,7 @@ enum class level_state
 
 namespace level_consts
 {
-	constexpr float TIMER = 30.0f;
+	constexpr float TIMER = 100000.0f;
 	constexpr float START_TIMER = 3.0f;
 	const static std::string COUNTDOWN_TEXT = "3";
 	const static std::string COUNTDOWN_FONT_NAME = "gill_sans_mt_bold_144";
