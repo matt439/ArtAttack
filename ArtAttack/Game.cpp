424e8218edc8d64f20a8a5717a861457dabe8738
matt439 <120173618+matt439@users.noreply.github.com>
2024-08-30 10:56:19 +1000

    set fixed time step

--- ArtAttack/Game.cpp
+++ ArtAttack/Game.cpp
@@ -64,7 +64,7 @@ void Game::Initialize(GameData* game_data)
 
     // TODO: Change the timer settings if you want something other than the default variable timestep mode.
     // e.g. for 60 FPS fixed timestep update logic, call:
-    m_timer.SetFixedTimeStep(false);
+    m_timer.SetFixedTimeStep(true);
     m_timer.SetTargetElapsedSeconds(1.0 / 60);
 }
 
