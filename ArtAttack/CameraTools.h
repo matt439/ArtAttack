2388e9e8617af6a77d7afd4abf34f5412ec5f8b0
matt439 <120173618+matt439@users.noreply.github.com>
2024-08-30 12:36:41 +1000

    delete extra code

--- ArtAttack/CameraTools.h
+++ ArtAttack/CameraTools.h
@@ -15,37 +15,18 @@ namespace camera_consts
     constexpr float MIN_BORDER_TOP = 250.0f;
     constexpr float MIN_BORDER_RIGHT = 400.0f;
     constexpr float MIN_BORDER_BOTTOM = 250.0f;
-
 }
-
-//struct calculate_cam
-//{
-//    MattMath::Vector2F player_center = { 0.0f, 0.0f };
-//    MattMath::Vector2F viewport_size = { 0.0f, 0.0f };
-//    //MattMath::Vector2F border = { 0.0f, 0.0f };
-//    MattMath::Vector3F prev_camera = { 0.0f, 0.0f, 0.0f };
-//    //DirectX::SimpleMath::Rectangle level_bounds = { 0, 0, 0, 0 };
-//    MattMath::RectangleI camera_bounds = { 0, 0, 0, 0 };
-//    //MattMath::Vector2F tile_size = { 0.0f, 0.0f };
-//};
 
 class CameraTools
 {
 private:
-    //RECT calculate_camera_scroll_border(
-    //    const MattMath::Vector2F& viewport_size) const;
-
     border_thickness calculate_camera_scroll_border(
         const MattMath::Vector2F& viewport_size) const;
 public:
-    //MattMath::Vector3F calculate_camera(
-    //    const calculate_cam& params) const;
-
     MattMath::Camera calculate_camera(
         const MattMath::Vector2F& player_center,
         const MattMath::Vector2F& viewport_size,
         const MattMath::Camera& prev_camera,
         const MattMath::RectangleF& camera_bounds) const;
-
 };
 #endif
