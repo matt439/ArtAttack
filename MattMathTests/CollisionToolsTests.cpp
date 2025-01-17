d7d360dfdf822ae564ae40b008404be35065adf8
matt439 <120173618+matt439@users.noreply.github.com>
2025-01-17 16:56:23 +1000

    Adding tests for rectangle-triangle classification

--- MattMathTests/CollisionToolsTests.cpp
+++ MattMathTests/CollisionToolsTests.cpp
@@ -30,5 +30,24 @@ namespace CollisionToolsTests
 			Assert::AreEqual(-5.0f, amount.x, EPSILON_F);
 			Assert::AreEqual(-5.0f, amount.y, EPSILON_F);
 		}
+
+		TEST_METHOD(test_rectagle_triangle_collision)
+		{
+			RectangleF rect = RectangleF(220.0f, 5878.0f, 52.0f, 120.0f);
+			Triangle tri = Triangle(Vector2F(200.0f, 6000.0f), Vector2F(400.0f, 6000.0f), Vector2F(400.0f, 5800.0f));
+
+			collision_direction direction =
+				CollisionTools::calculate_object_collision_direction(&rect, &tri);
+
+			Assert::AreEqual(static_cast<int>(collision_direction::BOTTOM_RIGHT), 
+				static_cast<int>(direction));
+
+			Vector2F amount = Vector2F(0.0f, 0.0f);
+			bool resolved = CollisionTools::resolve_object_collision(&rect, &tri, direction, amount);
+
+			Assert::IsTrue(resolved);
+			Assert::AreEqual(-1.0f, amount.x, EPSILON_F);
+			Assert::AreEqual(-1.0f, amount.y, EPSILON_F);
+		}
 	};
 }
